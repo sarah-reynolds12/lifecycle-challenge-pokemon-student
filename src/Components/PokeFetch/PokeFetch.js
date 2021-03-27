d75957d1061fd76bb793e340aee72c2c32ff9ce3
sarah-reynolds12 <73564744+sarah-reynolds12@users.noreply.github.com>
2021-03-27 17:17:05 -0400

    pokemon

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -9,6 +9,8 @@ class PokeFetch extends Component {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+      secondsRemaining: 10,
+      showPokemon: false  
     }
   }
 
@@ -24,9 +26,23 @@ class PokeFetch extends Component {
           pokeInfo: res,
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
+          secondsRemaining: 10
         })
       })
       .catch((err) => console.log(err))
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(this.state.pokeName !== prevState.pokeName) {
+      this.timer();
+    }
+  }
+  timer= () => {
+    let newTimer= setInterval(tick, 1000)
+    const tick = () => {
+
+    }
+    console.log('timer started')
   }
 
   render() {
@@ -35,8 +51,8 @@ class PokeFetch extends Component {
         <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
         <h1 className={'timer'} >Timer Display</h1>
         <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
+          <img className={'pokeImg'} style={this.state.showPokemon ? {filter: "brightness(100%)"}: {filter: 'brightness(0%)'}} src={this.state.pokeSprite} />
+          <h1 className={'pokeName'} style={{display:'none'}}>{this.state.pokeName}</h1>
         </div>
       </div>
     )
